04c9f930b74e4349a213e80c7aa382328bf284db
naimur <naimur202163@gmail.com>
2022-08-06 08:53:07 +0600

    int data types

--- module/class2/oprator.c
+++ module/class2/oprator.c
@@ -5,5 +5,13 @@ int main()
     int num1, num2;
     scanf("%d %d", &num1, &num2);
     int sum = num1 + num2;
-    printf("%d+%d=%d", num1, num2, sum);
+    int minus = num1 - num2;
+    int mul = num1 * num2;
+    int div = num1 / num2;
+    int vhagshesh = num1 % num2;
+    printf("%d %% %d=%d\n", num1, num2, vhagshesh);
+    printf("%d/%d=%d\n", num1, num2, div);
+    printf("%d*%d=%d\n", num1, num2, mul);
+    printf("%d-%d=%d\n ", num1, num2, minus);
+    printf("%d+%d=%d\n", num1, num2, sum);
 }
