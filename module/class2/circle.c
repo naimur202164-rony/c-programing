7adc72a4c1520e93afb4db9173965f854ef70209
naimur <naimur202163@gmail.com>
2022-08-06 10:01:24 +0600

    ara of circle is added

---
+++ module/class2/circle.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int r;
+    scanf("please give the r : %d");
+
+    float perimeter = 6.28;
+    float area = 3.14;
+
+    int result = perimeter * area * r;
+
+    printf("Area of circle  :  %d", result);
+}
---
+++ module/class2/opratorr.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int number = 10;
+
+    double realnumber = number;
+    printf("number =%d ,realnumber=%lf", number, realnumber);
+
+    // int price;
+    // price = 20;
+
+    // printf("price= %d\n", price);
+
+    // price = 40;
+
+    // printf("price= %d\n", price);
+}
