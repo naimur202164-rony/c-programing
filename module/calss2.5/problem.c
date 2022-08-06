f6227b01cb084811c91adaeb2a1544777b1c4cff
naimur <naimur202163@gmail.com>
2022-08-06 22:12:51 +0600

    problem is solved

--- module/calss2.5/problem.c
+++ module/calss2.5/problem.c
@@ -2,5 +2,17 @@
 
 int main()
 {
-    printf("Hello World");
+    int a, b, c, result;
+
+    printf("Enter the a :  \n");
+    scanf("%d", &a);
+    printf("Enter the b  \n", b);
+    scanf("%d", b);
+
+    result = a * b;
+    printf("The Area is  %d\n", result);
+    int sum = a + b;
+    float total = 2 * sum;
+
+    printf("The total Area is %f\n", total);
 }
