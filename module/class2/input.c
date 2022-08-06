f0c1aaefc4f91c1cee21e2482013e3e0459101de
naimur <naimur202163@gmail.com>
2022-08-06 08:36:58 +0600

    oparator added

--- module/class2/input.c
+++ module/class2/input.c
@@ -6,14 +6,16 @@ int main()
     int marks_math;
     printf("Enter Marks in English : ");
     scanf("%d", &marks_english);
-    printf("Enter Marks in Math");
-    scanf("%d", marks_math);
+    printf("Enter Marks in Math :  ");
+    scanf("%d", &marks_math);
     // int total =
-    marks_english = 50;
-    marks_math = 70;
+
     // scanf("%d", marks_english);
     int total = marks_english + marks_math;
-
     printf("marks=%d\n", total);
+
+    float temp;
+    printf("Enter Temp");
+    scanf("%f\n", temp);
     return 0;
 }
---
+++ module/class2/oprator.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1, num2;
+    scanf("%d %d", &num1, &num2);
+    int sum = num1 + num2;
+    printf("%d+%d=%d", num1, num2, sum);
+}
