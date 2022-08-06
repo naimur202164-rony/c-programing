350c1f676573060cf955927d55b4e3695b821100
naimur <naimur202163@gmail.com>
2022-08-06 21:36:46 +0600

    problem2 solved

--- module/calss2.5/index.c
+++ module/calss2.5/index.c
@@ -10,10 +10,19 @@
 //     return 0;
 // }
 
+// int main()
+// {
+//     int sum;
+//     sum = 25 + 37 - 19;
+//     printf("The answer is %d\n", sum);
+//     return 0;
+// }
+
 int main()
 {
-    int sum;
-    sum = 25 + 37 - 19;
-    printf("The answer is %d\n", sum);
-    return 0;
+    printf("HEllo\n");
+    printf("nice\n");
+    printf("to\n");
+    printf("meet\n");
+    printf("you\n");
 }
