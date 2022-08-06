5bd429fb7b90b82ea2a1c853750dd43a37664048
naimur <naimur202163@gmail.com>
2022-08-06 21:33:45 +0600

    error solved

--- module/calss2.5/index.c
+++ module/calss2.5/index.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 
+// int main()
+// {
+
+//     printf("Testing....");
+//     printf("....1");
+//     printf("..3");
+//     printf("\n");
+//     return 0;
+// }
+
 int main()
 {
-
-    printf("Testing....");
-    printf("....1");
-    printf("..3");
-    printf("\n");
+    int sum;
+    sum = 25 + 37 - 19;
+    printf("The answer is %d\n", sum);
     return 0;
 }
