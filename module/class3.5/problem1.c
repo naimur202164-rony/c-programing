1bc811eced0eea8c3d44c4f57a92841228bd1367
naimur <naimur202163@gmail.com>
2022-08-09 08:47:07 +0600

    problem1 solved

---
+++ module/class3.5/problem1.c
@@ -0,0 +1,16 @@
+// 1:Write a C program to print your name, date of birth. and mobile number.
+
+#include <stdio.h>
+
+int main()
+{
+    int date, number;
+
+    char name[20] = "NaimuurRahman";
+    number = 192019;
+    date = 9 / 8 / 2022;
+
+    printf("Name: %s\n", name);
+    printf("DOB : %d\n", date);
+    printf("Phone : %d\n", number);
+}
