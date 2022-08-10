3b046d563171eb0b49169d50202df3ba84fd1620
naimur <naimur202163@gmail.com>
2022-08-10 11:21:04 +0600

    problem 6 is solved

---
+++ module/class4/problem6.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+int main(){
+    int age,avarage,babul;
+if(-1000>age>1000 && avarage){
+    scanf("%d",&age);
+    scanf("%d",&avarage);
+}else(
+    printf("Not a number")
+);
+
+    babul=(avarage*2)-age;
+    printf("babul %d",babul);
+
+}
+
+
