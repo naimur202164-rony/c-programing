4cc9530c8ed9db3fe6a365eb6cc735d502ae4ca4
naimur <naimur202163@gmail.com>
2022-08-10 07:07:50 +0600

    arrey

--- module/class4/arr.c
+++ module/class4/arr.c
@@ -2,8 +2,16 @@
 
 int main()
 {
+    int mark[3];
     int mark1, mark2;
+    for (int i = 0; i < 3; i++)
+    {
+        printf("Enter the Student %dMark ", i + 1);
+        scanf("%d\n", &mark[i]);
+    }
 
-    printf("Enter the Student 1 Mark ");
-    scanf("%d\n", &mark1)
+    for (int i = 0; i < 3; i++)
+    {
+        printf("Student %d  go %d mark\n", i + 1, mark[i]);
+    }
 }
