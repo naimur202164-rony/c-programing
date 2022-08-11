110dfaa0c8d4bce7e8bba25865f86bcf5c6b511a
naimur <naimur202163@gmail.com>
2022-08-11 20:53:15 +0600

    working with condition

---
+++ module/class4.1/if.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int marks;
+    scanf("%d", &marks);
+
+    if (marks >= 40)
+    {
+        printf("You have passed");
+    }
+    else
+    {
+        printf("You have faild")
+    };
+}
