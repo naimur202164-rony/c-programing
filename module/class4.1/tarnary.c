6a6e3c7247e9b8a588ae79664d39ea03eafe8f79
naimur <naimur202163@gmail.com>
2022-08-12 07:01:43 +0600

    tarnary oprator added

--- module/class4.1/tarnary.c
+++ module/class4.1/tarnary.c
@@ -4,12 +4,8 @@ int main(){
     int num;
     scanf("%d",&num);
 
-    int abs;
-    if(num>=0){
-        abs=num;
-    }else{
-        abs=-num;
-    }
+
+int abs=num>=0?num:-num;
 
     printf("%d",abs);   
 }
